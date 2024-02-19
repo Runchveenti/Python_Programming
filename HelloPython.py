580645e620d8b52cacf7b8a1f90865097635230d
Runchveenti <nguyenngocanhthufpt77@gmail.com>
2024-02-19 16:26:46 +0700

    update

--- HelloPython.py
+++ HelloPython.py
@@ -1,4 +1,18 @@
-# My first Python code
-print("Hello")
+for i in range(1, 101):
+    print(i)
+    if i % 10 == 0:
+        print()
 
-
+# Nhap 1 nam duong lich, in ra nam am lich
+# 2024 -> Giap Thin
+can = ['Canh', 'Tan', 'Nham', 'Quy', 'Giap', 'At', 'Binh', 'Dinh', 'Mau', 'Ky']
+chi = ['Than', 'Dau', 'Tuat', 'Hoi', 'Ty', 'Suu', 'Dan', 'Meo', 'Thin', 'Ty', 'Ngo', 'Mui']
+# Nhap nam duong lich
+namDL = int(input("Nhap nam duong lich:"))
+# Tinh can
+can1 = namDL % 10
+# Tinh chi
+chi1 = namDL % 12
+# Doi nam AL
+namAL = can[can1] + " " + chi[chi1]
+print(namAL)
